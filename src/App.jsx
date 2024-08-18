87637d7fa4b041c5c6ec64d123191ced3602677e
Vinik <56496329+Vn1k@users.noreply.github.com>
2024-08-18 20:59:42 +0700

    galeri meliora

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import pages
 import HariPelaksanaan from "./pages/HariPelaksanaan/HariPelaksanaan";
 import Error from "./pages/error/Error";
+import Galeri from "./pages/galeri-meliora/galeri"
 
 // import components
 import NavigationBar from "./components/navbar/NavigationBar";
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HariPelaksanaan />} />
           <Route path="*" element={<Error />} />
+          <Route path="/galeri" element={<Galeri />} />
         </Routes>
         <NavigationBar />
       </Router>
