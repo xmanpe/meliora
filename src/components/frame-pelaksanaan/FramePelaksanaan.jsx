0ae1bb86ae91f4d4fb0444fcd85831627e274560
JoseAndreasLie <jose.lie2208@gmail.com>
2024-08-25 21:49:33 +0700

    Update Hari Pelaksanaan :cry:

--- src/components/frame-pelaksanaan/FramePelaksanaan.jsx
+++ src/components/frame-pelaksanaan/FramePelaksanaan.jsx
@@ -4,55 +4,52 @@ import React from "react";
 import "./FramePelaksanaan.scss";
 
 // import images
-import Frame from '../../images/frame/frame.svg'
-import TestingPhoto from '../../images/frame/testing-photo.jpeg'
-
+import frame from "../../images/frame/FRAME MELIORA 3;2.png";
+import TestingPhoto from "../../images/frame/testing-photo.jpeg";
 
 const FramePelaksanaan = () => {
     return (
         <section className="frame-pelaksanaan">
-            <div className="each-frame">
-                <div className="border">
-                    {/* <img className="absolute-frame" src={Frame} alt="frame" /> */}
-                    <img src={TestingPhoto} alt="" />
+            <div className="container-HariPelaksanaan">
+                <div className="LeftDiv">
+                    <div className="SIO">
+                        {/* <img className="frame" src={frame} alt="" /> */}
+                        <img className="image" src={TestingPhoto} alt="SIO" />
+                        <p>Sesi Informasi OMB</p>
+                    </div>
                 </div>
-                <div className="text">
-                    <h1>Hari Pemupukan</h1>
-                    <p>12 Desember 2021</p>
+                <div className="MidDiv">
+                    <div className="TopDiv">
+                        <div className="HariPemupukan">
+                            {/* <img className="frame" src={frame} alt="" /> */}
+
+                            <img src={TestingPhoto} alt="Hari Pemupukan" />
+                            <p>Hari Pemupukan</p>
+                        </div>
+                    </div>
+                    <div className="BottomDiv">
+                        <div className="PerkembanganHari1">
+                            {/* <img className="frame" src={frame} alt="" /> */}
+                            <img src={TestingPhoto} alt="Perkembangan Hari 1" />
+                            <p>Perkembangan Hari 1</p>
+                        </div>
+                        <div className="PerkembanganHari2">
+                            {/* <img className="frame" src={frame} alt="" /> */}
+                            <img src={TestingPhoto} alt="Perkembangan Hari 2" />
+                            <p>Perkembangan Hari 2</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="each-frame">
-                <div className="border">
-                    {/* <img className="absolute-frame" src={Frame} alt="frame" /> */}
-                    <img src={TestingPhoto} alt="" />
-                </div>
-                <div className="text">
-                    <h1>Hari Pemupukan</h1>
-                    <p>12 Desember 2021</p>
-                </div>
-            </div>
-            <div className="each-frame">
-                <div className="border">
-                    {/* <img className="absolute-frame" src={Frame} alt="frame" /> */}
-                    <img src={TestingPhoto} alt="" />
-                </div>
-                <div className="text">
-                    <h1>Hari Pemupukan</h1>
-                    <p>12 Desember 2021</p>
-                </div>
-            </div>
-            <div className="each-frame">
-                <div className="border">
-                    {/* <img className="absolute-frame" src={Frame} alt="frame" /> */}
-                    <img src={TestingPhoto} alt="" />
-                </div>
-                <div className="text">
-                    <h1>Hari Pemupukan</h1>
-                    <p>12 Desember 2021</p>
+                <div className="RightDiv">
+                    <div className="STS">
+                        {/* <img className="frame" src={frame} alt="" /> */}
+                        <img src={TestingPhoto} alt="Sidang Terbuka Senat" />
+                        <p>Sidang Terbuka Senat</p>
+                    </div>
                 </div>
             </div>
         </section>
     );
-}
- 
+};
+
 export default FramePelaksanaan;
