975951d49df3d41aabaad2dd3b6fa6d30a059d0f
Daffa Akbari <xmanpe04@gmail.com>
2024-09-03 15:33:02 +0700

    Minor updates

--- src/components/carousel-divisi/CarouselDivisi.jsx
+++ src/components/carousel-divisi/CarouselDivisi.jsx
@@ -1,107 +1,75 @@
-import React, { useState } from "react";
-import { useContext, useEffect } from "react";
-import '../../index.scss';
+// CarouselDivisi.jsx
 
-// Import Swiper React components
+import React, { useState, useContext } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import './CarouselDivisi.scss';
-
-// import required modules
 import { Navigation, Keyboard, Pagination, Mousewheel, Autoplay, A11y } from 'swiper/modules';
-
 import carouselData from "./DataCarouselDivisi";
 import { DivisiContext } from '../../pages/FotoDivisi';
 
 const CarouselDivisi = () => {
-    const {activeIndex, setActiveIndex} = useContext(DivisiContext);
+    const { activeIndex, setActiveIndex } = useContext(DivisiContext);
     const [hideImages, setHideImages] = useState(false);
 
-    return(
-        <>
-            <div className='divisi-swiper'>
-                <Swiper
-                    slidesPerView={3}   
-                    centeredSlides={true}
-                    loop={true}         
-                    keyboard={{
-                        enabled: true,
-                    }}
-                    breakpoints={{
-                        1920: {
-                            spaceBetween: -100,
-                            slidesPerView: 5,
-                        },
-                        1440: {
-                            spaceBetween: -100,
-                            slidesPerView: 4,
-                        },
-                        1200: {
-                            spaceBetween: -100,
-                            slidesPerView: 3,
-                        },
-                        810: {
-                            spaceBetween: -100,
-                            slidesPerView: 3,
-                        },
-                        560:{
-                            spaceBetween: 16,
-                            slidesPerView: 3,
-                        },
-                        432:{
-                            spaceBetween: 16,
-                            slidesPerView: 1,
-                        },
-                        0:{
-                            spaceBetween: 0,
-                            slidesPerView: 1,
-                        }
-                    }}
-                    resistanceRatio={0.01}
-                    navigation={true}
-                    pagination={{ clickable: true }}
-                    scrollbar={{ draggable: true }}
-                    modules={[Navigation, Pagination, A11y, Mousewheel]}
-                    onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
-                    onSwiper={(swiper) => setActiveIndex(swiper.realIndex)}
-                    className="mySwiper"
-                >
-                    {
-                        carouselData["DIVISI"].map((carouselData, index)=>(
-                            <SwiperSlide>
-                                <div className="swiper-content">
-                                    <div className={`image-wrapper-${hideImages ? 'mini' : ''}`}>
-                                        {carouselData.foto.map((foto, imgIndex) => {
-                                            const classNames = ['left', 'middle', 'right'];
-                                            const className = classNames[imgIndex] || '';
-                                            return (
-                                                <div key={imgIndex} className={`${className}-frame-container`}>
-                                                    <img src={foto} alt={`${carouselData.nama} ${imgIndex + 1}`} className={className} />
-                                                </div>
-                                            );
-                                        })}
-                                    </div>
-                                    <div className="keterangan-divisi">
-                                        <img className="logo-divisi" src={carouselData.logo}/>
-                                        <div className="text-wrapper">
-                                            <h1 className="text-nama">{carouselData.nama}</h1>
-                                            <div className="text-divisi-wrapper">
-                                                <p className={`text-divisi-${carouselData.warna}`}>{carouselData.divisi}</p>
+    return (
+        <div className='divisi-swiper'>
+            <Swiper
+                slidesPerView={3}
+                centeredSlides={true}
+                loop={true}
+                keyboard={{ enabled: true }}
+                breakpoints={{
+                    1920: { spaceBetween: -100, slidesPerView: 5 },
+                    1440: { spaceBetween: -100, slidesPerView: 4 },
+                    1200: { spaceBetween: -100, slidesPerView: 3 },
+                    810: { spaceBetween: -100, slidesPerView: 3 },
+                    560: { spaceBetween: 16, slidesPerView: 3 },
+                    432: { spaceBetween: 16, slidesPerView: 1 },
+                    0: { spaceBetween: 0, slidesPerView: 1 },
+                }}
+                resistanceRatio={0.01}
+                navigation={true}
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                modules={[Navigation, Pagination, A11y, Mousewheel]}
+                onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+                onSwiper={(swiper) => setActiveIndex(swiper.realIndex)}
+                className="mySwiper"
+            >
+                {
+                    carouselData["DIVISI"].map((carouselData, index) => (
+                        <SwiperSlide key={index}>
+                            <div className="swiper-content">
+                                <div className={`image-wrapper-${hideImages ? 'mini' : ''}`}>
+                                    {carouselData.foto.map((foto, imgIndex) => {
+                                        const classNames = ['left', 'middle', 'right'];
+                                        const className = classNames[imgIndex] || '';
+                                        return (
+                                            <div key={imgIndex} className={`${className}-frame-container`}>
+                                                <img src={foto} alt={`${carouselData.nama} ${imgIndex + 1}`} className={className} />
                                             </div>
+                                        );
+                                    })}
+                                </div>
+                                <div className="keterangan-divisi">
+                                    <img className="logo-divisi" src={carouselData.logo} />
+                                    <div className="text-wrapper">
+                                        <h1 className="text-nama">{carouselData.nama}</h1>
+                                        <div className="text-divisi-wrapper">
+                                            <p className={`text-divisi-${carouselData.warna}`}>{carouselData.divisi}</p>
                                         </div>
                                     </div>
                                 </div>
-                            </SwiperSlide>
-                        ))
-                    }
-                </Swiper>
-            </div>
-        </>
-    )
-}
+                            </div>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+        </div>
+    );
+};
 
-export default CarouselDivisi
+export default CarouselDivisi;
--- src/pages/FotoDivisi.jsx
+++ src/pages/FotoDivisi.jsx
@@ -1,31 +1,66 @@
-import React from "react";
-import { createContext, useState, useEffect } from "react";
+// FotoDivisi.jsx
 
+import React, { createContext, useState, useEffect } from "react";
 import './FotoDivisi.scss';
-
-//import background
 import Header from '../components/header/Header';
 import CarouselDivisi from "../components/carousel-divisi/CarouselDivisi";
 import DokumentasiDivisi from "../components/dokumentasi-divisi/DokumentasiDivisi";
 import PhotocardDivisi from "../components/photocard-divisi/PhotocardDivisi";
-import SectionFoto from "../components/section-foto/sectionFoto"
+import SectionFoto from "../components/section-foto/sectionFoto";
+import carouselData from '../components/carousel-divisi/DataCarouselDivisi';
 
-//import images
-import MelioraYellow from '../images/logo/meliora yellow.svg'
+// Import images
+import MelioraYellow from '../images/logo/meliora yellow.svg';
+import gandaraImage from '../images/background/divisi/gandara.jpeg';
+import baskaraImage from '../images/background/divisi/baskara.jpeg';
+import caksanaImage from '../images/background/divisi/caksana.jpeg';
+import nayanikaImage from '../images/background/divisi/nayanika.jpeg';
+import gajendraImage from '../images/background/divisi/gajendra.jpeg';
+import sakhaImage from '../images/background/divisi/sakha.jpeg';
+import lavanyaImage from '../images/background/divisi/lavanya.jpeg';
+import bimasenaImage from '../images/background/divisi/bimasena.jpeg';
+import sarayaImage from '../images/background/divisi/saraya.jpeg';
+import sarkaraImage from '../images/background/divisi/sarkara.jpeg';
+import balwanaImage from '../images/background/divisi/balwana.jpeg';
 
 export const DivisiContext = createContext();
+
+const gradients = {
+    biru: `url(${gandaraImage})`,
+    merah: `url(${balwanaImage})`,
+    abu: `url(${caksanaImage})`,
+    toska: `url(${lavanyaImage})`,
+    hitam: `url(${bimasenaImage})`,
+    oranye: `url(${baskaraImage})`,
+    ungu: `url(${gajendraImage})`,
+    pink: `url(${sakhaImage})`,
+    kuning: `url(${nayanikaImage})`,
+    cokelat: `url(${sarkaraImage})`,
+    hijau: `url(${sarayaImage})`,
+};
 
 export const FotoDivisi = ({ children }) => {
     useEffect(() => {
         window.scrollTo(0, 0);
-      }, []);
+    }, []);
 
     const [activeIndex, setActiveIndex] = useState(0);
     const photocardIndex = [0, 5, 8];
-    
+    const activeDivisi = carouselData["DIVISI"][activeIndex];
+    const gradientColor = gradients[activeDivisi?.warna] || 'none';
+
     return (
         <DivisiContext.Provider value={{ activeIndex, setActiveIndex }}>
-            <div className="foto-divisi_section">
+            <div
+                className="foto-divisi_section"
+                style={{
+                    backgroundImage: `${gradientColor}, url(${activeDivisi?.image || ''})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundAttachment: 'fixed',
+                }}
+            >
                 <Header
                     image={MelioraYellow}
                     top='Foto Divisi'
